44a82a1541b7a5d817c33708ebf6c4ddc79a38dc
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2022-11-07 19:14:40 +0600

    Add files via upload

---
+++ HotelTransylvania/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelTransylvania.Models;
+using System.Web.Security;
+
+namespace HotelTransylvania.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new LoginAspEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Contact");
+                }
+
+                ModelState.AddModelError("", "Invalid Username & Password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new LoginAspEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+                return RedirectToAction("login");
+        }
+    }
+}
